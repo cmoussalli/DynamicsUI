7c781d59950abd11eb6d73d301a4efaad88d782c
Caesar Moussalli <cmoussalli@gmail.com>
2023-08-12 18:38:09 +0400

    Add Demo Project & Navbar Style

---
+++ DynamicsUI/Components/DynNavBar.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicsUI.Components
+{
+    public partial class DynNavBar
+    {
+
+        [Parameter]
+        public Color BackgroundColor { get; set; } = Color.Navy;
+
+        [Parameter]
+        public string Title { get; set; }
+
+
+
+    }
+}
