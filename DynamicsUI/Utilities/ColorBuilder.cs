ca6c7aef149a761216b56d9023558eb036a6eada
Caesar Moussalli <cmoussalli@gmail.com>
2023-08-14 23:08:15 +0400

    ColorBuilder

---
+++ DynamicsUI/Utilities/ColorBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicsUI.Utilities
+{
+    public static class ColorBuilder
+    {
+        public static string ConvertToString(Color color)
+        {
+            return $"rgb({ color.R.ToString()},{color.G.ToString()},{color.B.ToString()})";
+        }
+    }
+}
